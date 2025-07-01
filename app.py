9fb8a443aa784f29c782cd20e9010557502b6466
sewnjean <161675937+sewnjean@users.noreply.github.com>
2025-07-01 23:08:45 +0800

    mmm

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import streamlit as st
 import pandas as pd
+import pickle
 from sklearn.model_selection import train_test_split
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.naive_bayes import MultinomialNB
